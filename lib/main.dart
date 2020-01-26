418ce5d8da6cd270a59855cba56f8c9690580bdb
alsoxavi <oscarj9410@gmail.com>
2020-01-26 15:48:46 -0500

    c27_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main () {
-	runApp(MyBasicApp());
-}
+//void main () {
+//	runApp(MyBasicApp());
+//}
+
+void main() => runApp(MyBasicApp());
 
 class MyBasicApp extends StatelessWidget {
+	@override
 	Widget build(BuildContext context) {
-		return MaterialApp(home: Text('Hello!'),);
+		return MaterialApp(
+			home: Scaffold(
+				appBar: AppBar(
+					title: Text('App Tittle'),
+				), // AppBar
+				body: Text('This is the body of the app'),
+			), // Scaffold
+		); // MaterialApp
 	}
 }
